da85d65d2745f837c62b850c69be41d908418d33
velocitatem <daniel@alves.world>
2023-11-22 15:27:47 +0100

    gave stufff cool

--- main.c
+++ main.c
@@ -4,9 +4,32 @@
 #include <lexer.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <ctype.h>
 
-int main() {
-    const char* source = "hello world 1299223";
+
+// load file thats the first argument
+// if no argument, load from stdin
+int main(int argc, char** argv) {
+
+    // chekc if file is in .rem extension
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    char *filename = argv[1];
+    const char* source;
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("Error: File not found\n");
+        exit(EXIT_FAILURE);
+    }
+    // read file line by line
+    while ((read = getline(&line, &len, fp)) != -1) {
+        source = line;
+    }
+    fclose(fp);
+
     Lexer *lex = initLexer(source);
 
     Token  token;
@@ -15,6 +38,8 @@ int main() {
         // Process the token, like printing it
     } while (token.coreToken != TOKEN_EOF);
 
+    freeLexer(lex);
+    free(line);
     // Free token values and perform any necessary cleanup
     return 0;
 }
